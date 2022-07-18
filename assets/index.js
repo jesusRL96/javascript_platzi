8161ff02292aa1438bacb20fbca07c757a3e87f5
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-07-18 11:58:52 -0500

    Los métodos call, apply y bind

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,32 @@
 import MediaPlayer from "./mediaPlayer.js";
 const video = document.querySelector('video');
-const button = document.querySelector('button');
+const button = document.querySelector('button#play-button');
 
 const player = new MediaPlayer({video});
 button.onclick = () => player.togglePlay();
+
+// call
+function saludar() {
+    console.log(`hola, soy ${this.nombre} ${this.apellido}`);
+}
+const persona = {nombre:'juan', apellido:'lopez'}
+saludar.call(persona);
+
+function caminar(metros, direccion) {
+    console.log(`${this.nombre}: caminar ${metros} -> ${direccion}`);
+}
+caminar.call(persona, 300, 'norte');
+// apply
+caminar.apply(persona, [200, 'sur'])
+// bind
+const persona1 = {nombre:'jorge', apellido:'lopez'}
+const saludoPersona1 = saludar.bind(persona1);
+saludoPersona1();
+const persona1Camina = caminar.bind(persona1)
+persona1Camina(200,'este');
+
+const buttons = document.getElementsByClassName('boton-call');
+// buttons.forEach(button => {
+Array.prototype.forEach.call(buttons, (button,index) => {
+    button.onclick = (button) => alert('call: '+index);
+});
