2a75c1fc94c66b731185bb040b8fe23ec8cbf307
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-07-26 13:34:37 -0500

    IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,88 +1,21 @@
-import MediaPlayer from "./mediaPlayer.js";
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
 const video = document.querySelector('video');
-const button = document.querySelector('button#play-button');
-
-const player = new MediaPlayer({video});
-button.onclick = () => player.togglePlay();
-
-// call
-function saludar() {
-    console.log(`hola, soy ${this.nombre} ${this.apellido}`);
-}
-const persona = {nombre:'juan', apellido:'lopez'}
-saludar.call(persona);
-
-function caminar(metros, direccion) {
-    console.log(`${this.nombre}: caminar ${metros} -> ${direccion}`);
-}
-caminar.call(persona, 300, 'norte');
-// apply
-caminar.apply(persona, [200, 'sur'])
-// bind
-const persona1 = {nombre:'jorge', apellido:'lopez'}
-const saludoPersona1 = saludar.bind(persona1);
-saludoPersona1();
-const persona1Camina = caminar.bind(persona1)
-persona1Camina(200,'este');
-
-const buttons = document.getElementsByClassName('boton-call');
-// buttons.forEach(button => {
-Array.prototype.forEach.call(buttons, (button,index) => {
-    button.onclick = (button) => alert('call: '+index);
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 
-// Prototype
-function Persona(name) {
-    // const persona = Object.create(Persona.prototype)
-    // persona.name = name;
-    // persona.saludar = function() {console.log(`hola soy ${this.name}`)};
-    // return persona;
-    this.name = name
-}
-Persona.prototype.saludar = function() {console.log(`hola soy ${this.name}`)};
-const juan = new Persona('juan obj');
-console.log('hasOwnProperty',juan.hasOwnProperty('name'), juan.name);
-console.log('hasOwnProperty',juan.hasOwnProperty('saludar'), juan.saludar);
-console.log('hasOwnProperty',juan.hasOwnProperty('toString'), juan.toString);
-// https://esprima.org/
-// https://astexplorer.net/
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
-// Generadores
-function* gen() {
-    yield 1;
-    yield 2;
-    yield 3;
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
   }
-
-// var g = gen(); // "Generator { }"
-
-function* idMakerWithReset() {
-    let id = 1
-    let reset
-    while(true) {
-        reset = yield id
-        if (reset) {
-        id = 1
-        } else {
-        id += 1
-        }
-    }
-}
-
-// const idMR = idMakerWithReset()
-// idMR.next() //f.next(true)
-
-function* fibo() {
-    let s1 = 0
-    let s2 = 1
-    let nextNumber = 1
-    while(true) {
-      yield nextNumber
-      nextNumber = s1 + s2
-      s1 = s2
-      s2 = nextNumber
-    }
-  }
-
-// const f = fibo()
-// f.next()
+};
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -1,19 +1,50 @@
-class MediaPlayer {
-    constructor(player) {
-        this.video = player.video;
-    }
-    play() {
-        this.video.play();
-    }
-    pause() {
-        this.video.pause();
-    }
-    togglePlay(){
-        if(this.video.paused){
-            this.video.play()
-        }else{
-            this.video.pause()
-        }
-    }
+function MediaPlayer(config) {
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function() {
+  const player = {
+    play: () => this.play(),
+    pause: () => this.pause(),
+    media: this.media,
+    get muted() {
+      return this.media.muted;
+    },
+    set muted(value) {
+      this.media.muted = value;
+    },
+  };
+
+  this.plugins.forEach(plugin => {
+    plugin.run(player);
+  });
+};
+
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+};
+
 export default MediaPlayer;
