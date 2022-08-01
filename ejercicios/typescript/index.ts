b6a41680e2339c0f3040799d19cd0de8592c707a
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-08-01 17:19:43 -0500

    Funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -24,3 +24,26 @@ enum Color {
 }
 let colorFavorito: Color = Color.Verde;
 console.log(`Color favorito ${colorFavorito}`);
+
+// Functions
+function add(a: number, b: number): number  {
+    return a + b;
+}
+const sum = add(10,15)
+
+function createAdder(a: number): (number) => number {
+    return function (b: number): number {
+        return a + b
+    }
+}
+
+const addFour = createAdder(4);
+const sumAdder = addFour(6);
+
+function fullName(firstName: string, lastName?: string, age: number = 0): string {
+    return `${firstName} ${lastName} (${age})`;
+}
+
+const yo = fullName('jesus', 'rl', 20);
+const def = fullName('pepe');
+console.log(yo);
