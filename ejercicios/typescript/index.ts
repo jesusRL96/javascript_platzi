45bdd7b4cd7dac8f7fc83552bb1fc1bd09707c77
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-08-01 17:05:14 -0500

    Tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,26 @@
-console.log('hola');
+// boolean
+let muted: boolean = true;
+muted = false;
+// Number
+let numerador: number = 254;
+let denominator: number = 200;
+let resultado = numerador / denominator;
 
-function add(a:number, b:number) {
-    return a + b;
+// String
+let nombre: string = 'Jesus';
+
+// arrays
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Pepe"]
+people.push(1);
+
+let strAndNbum: Array<string | number> = [];
+strAndNbum.push('Juan');
+strAndNbum.push(1);
+
+// Enum
+enum Color {
+    Rojo='Rojo', Verde='Verde', Azul='Azul'
 }
-
-const sum = add(3,6);
+let colorFavorito: Color = Color.Verde;
+console.log(`Color favorito ${colorFavorito}`);
